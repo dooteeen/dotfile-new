3a7e671ed2679bb98f51bf0f0fabadedb815fc4b
dooteeen <do2te3n@gmail.com>
2022-08-21 22:34:44 +0900

    update.

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -40,44 +40,80 @@ opt.whichwrap = 'b,s,h,l,<,>,[,]'
 opt.backspace = 'indent,eol,start'
 opt.fileformats = 'dos,unix,mac'
 
-opt.helplang = 'ja','en'
+opt.helplang = {'ja','en'}
+
+opt.showtabline = 0
+
+opt.splitbelow = true
+opt.splitright = true
 
 opt.updatetime = 300
 opt.signcolumn = 'yes'
 
 
 
+key("n", "gj", "j",  { noremap = true, silent = true })
+key("n", "j",  "gj", { noremap = true, silent = true })
+key("n", "gk", "k",  { noremap = true, silent = true })
+key("n", "k",  "gk", { noremap = true, silent = true })
+
+key("n", "s", "<Nop>", { noremap = true, silent = true })
+key("n", "S", "<Nop>", { noremap = true, silent = true })
+key("v", "s", "<Nop>", { noremap = true, silent = true })
+key("v", "S", "<Nop>", { noremap = true, silent = true })
+
+key("n", "q", "$",     { noremap = true, silent = true })
+key("n", "C", "<Nop>", { noremap = true, silent = true })
+key("o", "q", "$",     { noremap = true, silent = true })
+key("o", "C", "<Nop>", { noremap = true, silent = true })
+key("v", "q", "$",     { noremap = true, silent = true })
+key("v", "C", "<Nop>", { noremap = true, silent = true })
+
+key("n", "Q", "q", { noremap = true, silent = true })
+
+key("n", "t", "0", { noremap = true, silent = true })
+key("n", "T", "^", { noremap = true, silent = true })
+key("o", "t", "0", { noremap = true, silent = true })
+key("o", "T", "^", { noremap = true, silent = true })
+key("v", "t", "0", { noremap = true, silent = true })
+key("v", "T", "^", { noremap = true, silent = true })
+
+key("n", "<", "<h",  { noremap = true, silent = true })
+key("n", ">", ">l",  { noremap = true, silent = true })
+key("v", "<", "<gv", { noremap = true, silent = true })
+key("v", ">", ">gv", { noremap = true, silent = true })
+
+key("n", "<Tab>",   "<C-w>w", { noremap = true, silent = true })
+key("n", "<S-Tab>", "<C-w>W", { noremap = true, silent = true })
+
+key("n", "(", ":bp<CR>", { noremap = true, silent = true })
+key("n", ")", ":bn<CR>", { noremap = true, silent = true })
+
 key("n", "<Space>", "<Nop>", {})
-
-key("n", "gj", "j",  { noremap = true })
-key("n", "j",  "gj", { noremap = true })
-key("n", "gk", "k",  { noremap = true })
-key("n", "k",  "gk", { noremap = true })
-
-key("n", "q", "$", { noremap = true })
-key("n", "Q", "q", { noremap = true })
-key("n", "t", "0", { noremap = true })
-key("n", "T", "^", { noremap = true })
-key("v", "t", "0", { noremap = true })
-key("v", "T", "^", { noremap = true })
-
-key("n", "<", "<h",  { noremap = true })
-key("n", ">", ">l",  { noremap = true })
-key("v", "<", "<gv", { noremap = true })
-key("v", ">", ">gv", { noremap = true })
-
-key("n", "<Tab>",   "<C-w>w", { noremap = true })
-key("n", "<S-Tab>", "<C-w>W", { noremap = true })
-
-key("n", "(", ":bn<CR>", { noremap = true, silent = true })
-key("n", ")", ":bp<CR>", { noremap = true, silent = true })
+key("n", "~", "<Nop>", {})
+if not is_code then
+  if is_win then
+    default_term = "nyagos"
+  else
+    default_term = "fish"
+  end
+  key("n", "<Space>`", ":<C-u>tabnew | terminal "..default_term.."<CR>", { noremap = true })
+  key("n", "<Space>~", ":<C-u>tabnew | terminal "..default_term.."<CR>", { noremap = true })
+end
 
 
 
--- move cursor to latest modified place.
+user_augroup = 'user-init'
+api.nvim_create_augroup(user_augroup, { clear = false })
+api.nvim_create_autocmd({"TermOpen"}, {
+  group = user_augroup,
+  command = "startinsert",
+})
 api.nvim_create_autocmd({"BufReadPost"}, {
+  group = user_augroup,
   pattern = {"*"},
   callback = function()
+    -- move cursor to latest modified place.
     api.nvim_exec('silent! normal! g`"zv', false)
   end,
 })
@@ -110,7 +146,7 @@ require('packer').startup { function()
     branch = 'v2',
     config = function()
       require('hop').setup {
-        keys = 'asdfghjklwertyuiopvbn',
+        keys = 'adefghjklnoprstuvw',
         quit_key = 'q',
         multi_windows = true,
       }
@@ -127,13 +163,11 @@ require('packer').startup { function()
         sync_install = false,
         auto_install = true,
         ignore_install = { 'help', 'TelescopePrompt' },
+        endwise = { enable = true, },
         highlight = {
           enable = not is_code,
           disable = { 'help', 'TelescopePrompt' },
         },
-        endwise = {
-          enable = true,
-        }
       }
     end,
   }
@@ -143,7 +177,7 @@ require('packer').startup { function()
     requires = {{ 'nvim-treesitter/nvim-treesitter' }},
     config = function()
       require('tsht').config.hint_keys = {
-        "A","S","D","F","G","H","J","K","L"
+        "A", "B", "C", "D", "E", "F", "G",
       }
     end
   }
@@ -156,21 +190,101 @@ require('packer').startup { function()
   use {
     'kylechui/nvim-surround',
     config = function()
-      require('nvim-surround').setup {
-        aliases = {
-          ['Q'] = { [[']], [["]], [[`]] },
-          ['P'] = { '>', '}', ')', ']' },
-          ['S'] = { [[']], [["]], [[`]], '>', '}', ')', ']' }
-        }
+      require('nvim-surround').setup()
+    end
+  }
+
+  use {
+    'windwp/nvim-autopairs',
+    requires = {{ 'nvim-treesitter/nvim-treesitter' }},
+    config = function()
+      local autopairs = require('nvim-autopairs')
+      local rule = require('nvim-autopairs.rule')
+      -- local cond = require('nvim-autopairs.conds')
+      local endwise = require('nvim-autopairs.ts-rule').endwise
+      local smart_space = function(left, right)
+        return require('nvim-autopairs.rule')(left, right)
+          :with_pair(function() return false end)
+          :with_move(function(opts)
+            return opts.prev_char:match('.%'..right) ~= nil
+          end)
+          :use_key(right)
+      end
+      autopairs.setup { check_ts = true }
+      autopairs.add_rules {
+        -- add space between parentheses
+        rule(' ', ' '):with_pair(function(opts)
+          local p = opts.line:sub(opts.col - 1, opts.col)
+          return vim.tbl_contains({'()', '{}', '[]'}, p)
+        end),
+        smart_space('(', ')'),
+        smart_space('{', '}'),
+        smart_space('[', ']'),
+        -- for go-template (ex. chezmoi)
+        rule('{{', '}}'),
+        -- for fish shell
+        endwise('if.*$', 'end', 'fish', 'if_statement'),
+        endwise('for.*$', 'end', 'fish', 'for_statement'),
+        endwise('begin$', 'end', 'fish', 'begin_statement'),
+        endwise('while.*$', 'end', 'fish', 'while_statement'),
+        endwise('switch.*$', 'end', 'fish', 'switch_statement'),
+        endwise('function.*$', 'end', 'fish', 'function_definition'),
       }
     end
   }
 
 
-
   if is_code then return nil end
 
 
+  use {
+    'neovim/nvim-lspconfig',
+    requires = {{ 'williamboman/mason-lspconfig.nvim' }},
+    config = function()
+      local on_attach = function(client, bufnr)
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'vim.lua.vim.lsp.omnifunc')
+
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set('n', '<Space>1', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', '<Space>2', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', '<Space>3', vim.lsp.buf.references, bufopts)
+        vim.keymap.set('n', '<Space>4', vim.lsp.buf.formatting, bufopts)
+        vim.keymap.set('n', '<Space>5', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set('n', '<Space>6', vim.lsp.buf.code_action, bufopts)
+      end
+      local servers = { 'pyright' }
+      for _, lsp in pairs(servers) do
+        require('lspconfig')[lsp].setup {
+          on_attach = on_attach,
+          flags = { debounce_text_changes = 150 }
+        }
+      end
+    end
+  }
+
+  use {
+    'williamboman/mason.nvim',
+    config = function()
+      require('mason').setup {
+        icons = {
+          package_installed   = '[x]',
+          package_pending     = '[~]',
+          package_uninstalled = '[ ]',
+        }
+      }
+    end
+  }
+
+  use {
+    'williamboman/mason-lspconfig.nvim',
+    requires = {{ 'williamboman/mason.nvim' }},
+    config = function()
+      require('mason-lspconfig').setup {
+        automatic_installation = true,
+        ensure_installed = { 'sumneko_lua' },
+      }
+    end
+  }
 
   use 'gpanders/editorconfig.nvim'
 
@@ -192,11 +306,11 @@ require('packer').startup { function()
    config = function()
      require('gitsigns').setup {
        signs = {
-         add = { text = '|' }, -- green
-         change = { text = '|' }, -- blue
+         add = { text = '+' }, -- green
+         change = { text = '~' }, -- blue
          delete = { text = '_' }, -- red
          topdelete = { text = '^' }, -- red
-         changedelete = { text = '+' }, -- blue
+         changedelete = { text = '=' }, -- blue
        }
      }
    end
@@ -211,54 +325,6 @@ require('packer').startup { function()
     'nvim-treesitter/playground'
   }
 
-
-  vim.opt.signcolumn = 'yes'
-  use {
-    'windwp/nvim-autopairs',
-    requires = {{ 'nvim-treesitter/nvim-treesitter' }},
-    config = function()
-      local autopairs = require('nvim-autopairs')
-      local rule = require('nvim-autopairs.rule')
-      -- local cond = require('nvim-autopairs.conds')
-      local endwise = require('nvim-autopairs.ts-rule').endwise
-      autopairs.setup { check_ts = true }
-      autopairs.add_rules {
-        -- add space between parentheses
-        rule(' ', ' '):with_pair(function(opts)
-          local p = opts.line:sub(opts.col - 1, opts.col)
-          return vim.tbl_contains({'()', '{}', '[]'}, p)
-        end),
-        rule('(', ')')
-          :with_pair(function() return false end)
-          :with_move(function(opts)
-            return opts.prev_char:match('.%)') ~= nil
-          end)
-          :use_key(')'),
-        rule('{', '}')
-          :with_pair(function() return false end)
-          :with_move(function(opts)
-            return opts.prev_char:match('.%}') ~= nil
-          end)
-          :use_key('}'),
-        rule('[', ']')
-          :with_pair(function() return false end)
-          :with_move(function(opts)
-            return opts.prev_char:match('.%]') ~= nil
-          end)
-          :use_key(']'),
-        -- for lua
-        rule('[[', ']]', 'lua'),
-        -- for fish shell
-        endwise('if.*$', 'end', 'fish', 'if_statement'),
-        endwise('for.*$', 'end', 'fish', 'for_statement'),
-        endwise('begin$', 'end', 'fish', 'begin_statement'),
-        endwise('while.*$', 'end', 'fish', 'while_statement'),
-        endwise('switch.*$', 'end', 'fish', 'switch_statement'),
-        endwise('function.*$', 'end', 'fish', 'function_definition'),
-      }
-    end
-  }
-
   use {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
@@ -271,12 +337,16 @@ require('packer').startup { function()
 
   use {
     'nvim-lualine/lualine.nvim',
-    requires = {{ 'kyazdani42/nvim-web-devicons', opt = true }},
+    requires = {
+      { 'RRethy/nvim-base16' },
+      { 'kyazdani42/nvim-web-devicons', opt = true },
+    },
     config = function()
       require('lualine').setup {
         options = {
           icons_enabled = true,
-          theme = 'auto',
+          -- theme = is_win and 'onedark' or 'solarized_dark',
+          theme = 'base16',
           component_separators = '',
           section_separators = '',
         },
@@ -301,16 +371,6 @@ require('packer').startup { function()
     end
   }
 
-  use {
-    'mfussenegger/nvim-lint',
-    config = function()
-      require('lint').linters_by_ft = {
-        lua  = { 'luacheck' },
-        yaml = { 'yamllint' },
-      }
-    end
-  }
-
   if packer_bootstrap then
     require('packer').sync()
   end
@@ -320,12 +380,22 @@ end
 
 -- plugins keymap
 
-if not is_code and not packer_bootstrap then
+if not packer_bootstrap then
 
   if has_installed('kommentary.nvim') then
-    key("n", "--", "<Plug>kommentary_line_default", {})
-    key("n", "-",  "<Plug>kommentary_motion_default", {})
-    key("v", "-",  "<Plug>kommentary_visual_default<C-c>", {})
+    key('n', '--', '<Plug>kommentary_line_default', {})
+    key('n', '-',  '<Plug>kommentary_motion_default', {})
+    key('v', '-',  '<Plug>kommentary_visual_default<C-c>', {})
+  end
+
+  if has_installed('hop.nvim') then
+    key('n', '<Space>h', ':<C-u>HopPattern<CR>', {})
+    key('n', '<Space>l', ':<C-u>HopChar1<CR>', {})
+  end
+
+  if has_installed('nvim-treehopper') then
+    key('n', '<Space>t', function() require('tsht').move{side='start'} end, { noremap = true })
+    key('n', '<Space>T', function() require('tsht').move{side='end'}   end, { noremap = true })
   end
 
   if has_installed('telescope.nvim') then
@@ -349,43 +419,34 @@ if not is_code and not packer_bootstrap then
         })
       end, { noremap = true })
     end
-    if has_installed("hop.nvim") then
-      key("n", "<Space>h", ':<C-u>HopPattern<CR>', {})
-    end
-    if has_installed('nvim-treehopper') then
-      key("n", '<Space>t', function() require('tsht').move{side='start'} end, { noremap = true })
-      key("n", '<Space>T', function() require('tsht').move{side='end'}   end, { noremap = true })
-    end
   end
 
   if has_installed('nvim-base16') then
     local base16 = require('base16-colorscheme')
-    base16.with_config { telescope = true }
     local using_scheme = is_win and 'onedark' or 'solarized-dark'
+    base16.with_config { telescope = true }
     cmd("color base16-"..using_scheme)
-    -- overwrite colorscheme
-    api.nvim_set_hl(0, 'VertSplit', { fg = base16.colors.base02, bg = nil })
-    local color_assigns = {
-      { target = 'TSField', reference = 'TSVariable' },
-      { target = 'TSParameter', reference = 'TSVariable' },
-      { target = 'TSConstructor', reference = 'TSText' },
-      { target = 'TSPunctDelimiter', reference = 'TSText' },
-      { target = 'TSStringRegex', reference = 'TSConstant' },
-    }
-    for _, p in pairs(color_assigns) do
-      api.nvim_set_hl(0, p.target, { link = p.reference })
-    end
-  end
-
-  if has_installed('nvim-lint') then
-    api.nvim_create_autocmd({ 'BufWritePost' }, {
-      callback = function()
-        require('lint').try_lint()
-      end
-    })
-    require('lint.linters.luacheck').args = {
-      '--only', '0'
-    }
+    -- overwrite colorscheme with:
+    --   monotone: 00-07 (center: 3-4)
+    --   red:      08   orange:   09   yellow:   0A
+    --   green:    0B   cyan:     0C   blue:     0D
+    --   magenta:  0E   brown:    0F
+    local colors = base16.colors
+    api.nvim_set_hl(0, 'Cursor',    { fg = colors.base00, bg = colors.base0D })
+    api.nvim_set_hl(0, 'VertSplit', { fg = colors.base01, bg = nil })
+    -- overwrite colorscheme ... hop.nvim
+    api.nvim_set_hl(0, 'HopNextKey',   { fg = colors.base0E, bold = true })
+    api.nvim_set_hl(0, 'HopNextKey1',  { fg = colors.base0D, bold = true })
+    api.nvim_set_hl(0, 'HopNextKey2',  { fg = colors.base0C, bold = false })
+    api.nvim_set_hl(0, 'HopPreview',   { fg = colors.base00, bg = colors.base0A })
+    api.nvim_set_hl(0, 'HopUnmatched', { link = 'Comment' })
+    api.nvim_set_hl(0, 'HopCursor',    { link = 'Cursor' })
+    -- overwrite colorscheme ... nvim-treesitter
+    api.nvim_set_hl(0, 'TSField',          { link = 'TSVariable' })
+    api.nvim_set_hl(0, 'TSParameter',      { link = 'TSVariable' })
+    api.nvim_set_hl(0, 'TSConstructor',    { link = 'TSText' })
+    api.nvim_set_hl(0, 'TSPunctDelimiter', { link = 'TSText' })
+    api.nvim_set_hl(0, 'TSStringRegex',    { link = 'TSConstant' })
   end
 
 end
